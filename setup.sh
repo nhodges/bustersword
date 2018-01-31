ea89a932cb2b4245ced154b3288dcff20ae03157
Nuri Hodges <nuri@Nuris-MBP.lan>
2018-01-30 21:55:03 -0800

    feat(bustersword): install Node Version Manager

--- setup.sh
+++ setup.sh
@@ -3,5 +3,10 @@
 # Install: Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install: bashstrap
+# Install: Bashstrap
 cd; curl -#L https://github.com/barryclark/bashstrap/tarball/master | tar -xzv --strip-components 1 --exclude={README.md,screenshot.png}
+
+# Install: Node Version Manager
+brew install nvm
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+echo '. "/usr/local/opt/nvm/nvm.sh"' >> ~/.bashrc
